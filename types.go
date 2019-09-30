235afa0635927b5b64be384c1339ce61732df46b
Jack Wakefield <xadetthree@gmail.com>
2019-09-30 18:59:46 +0100

    Rename internal packages

--- types.go
+++ types.go
@@ -10,8 +10,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/denisenkom/go-mssqldb/internal/cp"
-	"github.com/denisenkom/go-mssqldb/internal/decimal"
+	"github.com/applinskinner/go-mssqldb/internal/cp"
+	"github.com/applinskinner/go-mssqldb/internal/decimal"
 )
 
 // fixed-length data types
